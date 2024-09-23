b450ba6cad9bf3670d12d93e63b75ecc61edef8d
oromadin <osromadin@gmail.com>
2024-09-23 15:40:26 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+      tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              clifford: '#da373d',
+            }
+          }
+        }
+      }
+    </script>
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline">
+      Практика #2
+    </h1>
+    <h1 class="text-3xl font-bold underline text-clifford">
+      Практика #2
+    </h1>
+    <button class="bg-green-500 text-white font-bold py-3 px-4 rounded-lg text-center">
+      Click Me
+    </button>
+</html>
