af7a523ecf716fe1977893168f0cf3f26da906dc
Oleg Romadin <osromadin@gmail.com>
2024-10-05 21:59:00 +0300

    dynamic avatar and logo urls

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ $products = [
     'alternativeCount' => 21,
     'productDescription' => 'CodeSandbox is an online code editor and prototyping tool that makes creating and sharing web apps faster',
     'lastReview' => 'Great updates, endless possibilities. My choice for coding small projects to share and have online for years!',
-    'lastReviewAuthorAvatarUrl' => '/img/avatar/henk.jpeg'
+    'lastReviewAuthorAvatarUrl' => '/img/avatar/henk.jpeg',
+    'productLogoUrl' => '/img/product-logo/codesandbox_logo.jpeg'
   ],
   [
     'productName' => 'Replit',
@@ -20,7 +21,8 @@ $products = [
     'alternativeCount' => 7,
     'productDescription' => 'Build software collaboratively with the power of AI, on any device, without spending a second on setup. Write code with Ghostwriter, an AI-powered pair programmer. Collaborate in our real-time multiplayer editor. Build, test, and deploy from the browser.',
     'lastReview' => null,
-    'lastReviewAuthorAvatarUrl' => null
+    'lastReviewAuthorAvatarUrl' => null,
+    'productLogoUrl' => '/img/product-logo/replit_logo.jpg'
   ],
   [
     'productName' => 'Codecademy',
@@ -28,7 +30,8 @@ $products = [
     'alternativeCount' => 59,
     'productDescription' => 'Codecademy, from Skillsoft, helps millions of learners create inspiring careers in technology through an interactive online platform and a growing catalog of courses, skill paths, and career paths.',
     'lastReview' => null,
-    'lastReviewAuthorAvatarUrl' => null
+    'lastReviewAuthorAvatarUrl' => null,
+    'productLogoUrl' => '/img/product-logo/codecademy_logo.jpeg'
   ],
   [
     'productName' => 'vscode.dev',
@@ -36,7 +39,8 @@ $products = [
     'alternativeCount' => 6,
     'productDescription' => 'Now when you go to https://vscode.dev, you\'ll be presented with a lightweight version of VS Code running fully in the browser. Open a folder on your local machine and start coding. No install required.',
     'lastReview' => null,
-    'lastReviewAuthorAvatarUrl' => null
+    'lastReviewAuthorAvatarUrl' => null,
+    'productLogoUrl' => '/img/product-logo/vscode_dev_logo.jpeg'
   ],
   [
     'productName' => 'adaface',
@@ -44,7 +48,8 @@ $products = [
     'alternativeCount' => 32,
     'productDescription' => 'Adaface is a skills assessments platform to identify qualified developers accurately using an AI chatbot with in-built code editor. Our bot, Ada can have deep technical discussions with engineering candidates about algorithms, architecture design and databases, just like a hiring manager does.',
     'lastReview' => null,
-    'lastReviewAuthorAvatarUrl' => null
+    'lastReviewAuthorAvatarUrl' => null,
+    'productLogoUrl' => '/img/product-logo/adaface_logo.jpeg'
   ],
   [
     'productName' => 'Tuple',
@@ -52,7 +57,8 @@ $products = [
     'alternativeCount' => 6,
     'productDescription' => 'Tuple is an app for remote pair programming, optimized for everything that developers care about: Seamless collaboration with full-time mouse and keyboard support for all parties, crystal-clear webcam and screen-sharing, CL integration and pluginability.',
     'lastReview' => 'I use Tuple every week to pair with engineers that are in completely different countries and don\'t tend to have any issues with screen sharing or discussing ideas! I love how simple the tool is too - it allows for simplicity while pairing.',
-    'lastReviewAuthorAvatarUrl' => '/img/avatar/kristine.jpeg'
+    'lastReviewAuthorAvatarUrl' => '/img/avatar/kristine.jpeg',
+    'productLogoUrl' => '/img/product-logo/tuple_logo.jpeg'
   ],
   [
     'productName' => 'CodeSandbox for iOS',
@@ -60,7 +66,8 @@ $products = [
     'alternativeCount' => 7,
     'productDescription' => ' CodeSandbox for iOS lets you take your projects and sandboxes with you wherever you go. Using an iPhone or iPad, you can now access our new cloud development platform in the comfort of a native iOS IDE.',
     'lastReview' => 'I\’ve been on the beta for the iOS app, and excited to it launch - I initially tried using the browser version on the iPad, which worked well, but this native iOS app is a game changer, as it brings all the native touch and gestures which the browser doesn\'t support. It just becomes so easy and much more comfortable to use. Now I can code without carrying the bigger laptop',
-    'lastReviewAuthorAvatarUrl' => '/img/avatar/peter.jpeg'
+    'lastReviewAuthorAvatarUrl' => '/img/avatar/peter.jpeg',
+    'productLogoUrl' => '/img/product-logo/codesandbox_for_ios_logo.jpeg'
   ],
 ];
 
@@ -77,6 +84,9 @@ $products = [
       <li class="flex flex-col">
         <div class="flex flex-col flex-wrap mx-11 my-5 left">
           <div class="flex flex-row gap-3">
+              <img class="rounded-full w-8 h-8"
+                   src="<?php echo $product['productLogoUrl'] ?>" 
+                   alt="<?php echo $product['productName']; ?>">
               <div class="text-16 font-semibold text-dark-gray">
                 <a target="_blank" href="/">
                   <?php echo $index + 1; ?>. <?php echo $product['productName']; ?>
