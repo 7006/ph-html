ce2d90d0c167351c8ce33e7137c435545f1e2664
Oleg Romadin <osromadin@gmail.com>
2024-10-10 00:53:23 +0300

    add more data

--- db/data.sql
+++ db/data.sql
@@ -24,7 +24,7 @@ values
   'Build software collaboratively with the power of AI, on any device, without spending a second on setup. Write code with Ghostwriter, an AI-powered pair programmer. Collaborate in our real-time multiplayer editor. Build, test, and deploy from the browser.',
   null,
   null,
-  '/img/product-logo/replit_logo.jpg'
+  '/img/product-logo/universal_logo.jpg'
   ),
   (
   'Codecademy',
@@ -58,7 +58,7 @@ values
   23,
   6,
   'Tuple is an app for remote pair programming, optimized for everything that developers care about: Seamless collaboration with full-time mouse and keyboard support for all parties, crystal-clear webcam and screen-sharing, CL integration and pluginability.',
-  'I use Tuple every week to pair with engineers that are in completely different countries and don''t tend to have any issues with screen sharing or discussing ideas! I love how simple the tool is too - it allows for simplicity while pairing.',
+  'I use Tuple every week to pair with engineers that are in completely different countries and dont tend to have any issues with screen sharing or discussing ideas! I love how simple the tool is too - it allows for simplicity while pairing.',
   '/img/avatar/kristine.jpeg',
   '/img/product-logo/tuple_logo.jpeg'
   ),
@@ -68,6 +68,231 @@ values
   7,
   'CodeSandbox for iOS lets you take your projects and sandboxes with you wherever you go. Using an iPhone or iPad, you can now access our new cloud development platform in the comfort of a native iOS IDE.',
   'I have been on the beta for the iOS app, and excited to it launch - I initially tried using the browser version on the iPad, which worked well, but this native iOS app is a game changer, as it brings all the native touch and gestures which the browser does not support. It just becomes so easy and much more comfortable to use. Now I can code without carrying the bigger laptop',
-    '/img/avatar/peter.jpeg',
-    '/img/product-logo/codesandbox_for_ios_logo.jpeg'
+  '/img/avatar/peter.jpeg',
+  '/img/product-logo/codesandbox_for_ios_logo.jpeg'
+  ),
+  (
+  'DigitalOcean',
+  42,
+  66,
+  'Helping millions of developers easily build, test, manage, and scale applications of any size faster than ever before.',
+  null,
+  null,
+  '/img/product-logo/digital_ocean_logo.jpeg'
+  ),
+  (
+  'Porter',
+  20,
+  19,
+  'Porter brings the magical experience of deploying an application with just a few clicks into your own AWS or GCP account. Porter will manage the underlying infrastructure for you - simply point at your repo and Porter will handle the rest, from building your application to autoscaling it.',
+  'Congratulations on the launch of Porter on Product Hunt! The ability to simplify the prototyping process and deploy applications into our own AWS accounts with ease is truly impressive. I have always valued user-friendly tools that streamline the design and development workflow, and Porter seems to do just that. The intuitive drag-and-drop interface and pre-built components make it incredibly convenient to create interactive prototypes, enabling us to bring our ideas to life quickly.',
+  '/img/avatar/atharva.jpeg',
+  '/img/product-logo/porter_logo.jpeg'
+  ),
+  (
+  'Render',
+  23,
+  25,
+  'A unified platform to build and run all your apps and websites with free SSL, a global CDN, private networks and auto deploys from Git.',
+  'The simplest place to host your products. Might not be the cheapest but my god does it save you time on setting things up',
+  '/img/avatar/kristian.jpeg',
+  '/img/product-logo/render_logo.jpeg'
+  ),
+  (
+  'USE Together',
+  5,
+  20,
+  'USE Together is a remote pair programming tool. Share your favorite IDE and apps, participants get their own mouse cursors and you are all in control. Onboard a new team member, debug a persisting issue in your code base, progress together or do remote pair programming interviews.',
+  null,
+  null,
+  '/img/product-logo/universal_logo.jpg'
+  ),
+  (
+  'Coderoulette',
+  2,
+  9,
+  'Pair programming game for developers.',
+  'In my opinion, Coderoulette is an exceptional platform that combines the thrill of coding challenges with the unpredictability of random pair programming. I find this quite entertaining as its unique approach of introducing unknown programming languages for each session not only keeps the experience fresh and exciting but also fosters a powerful environment for collaborative learning and mentorship for students like me. This concept  encourages adaptation and effective communication, making it an invaluable tool for anyone looking to enhance their coding skills while engaging with a diverse global community.',
+  '/img/avatar/ahmed.jpeg',
+  '/img/product-logo/coderoulette_logo.jpeg'
+  ),
+  (
+  'OpenVSCode Server',
+  4,
+  7,
+  'Run the latest VS Code on a remote machine accessed through a modern web browser - from any device, from anywhere. The open-source project is officially backed by GitLab, VMware, Uber, SAP, RStudio, Sourcegraph and many others.',
+  null,
+  null,
+  '/img/product-logo/universal_logo.jpg'
+  ),
+  (
+  'CodeCoach',
+  null,
+  2,
+  'Share a link and pair-program instantly.',
+  null,
+  null,
+  '/img/product-logo/code_coach_logo.jpeg'
+  ),
+  (
+  'YepCode',
+  12,
+  9,
+  'YepCode is a Saas platform that allows the development, execution and monitoring of integration and automation processes. Working directly with JavaScript, it provides all the out-of-the-box steroids that you need to solve the most complex problems.',
+  'We love the way yepcode makes it easy for us to keep our integrations controlled and maintainable. Our marketing and sales team can iterate and test new tools with almost no integration costs',
+  '/img/avatar/david.jpeg',
+  '/img/product-logo/universal_logo.jpg'
+  ),
+  (
+  'fastlane',
+  2,
+  6,
+  'The easiest way to build and release mobile apps. Fastlane handles tedious tasks so you do not have to.',
+  null,
+  null,
+  '/img/product-logo/fastlane_logo.jpeg'
+  ),
+  (
+  'Nimbus',
+  12,
+  11,
+  'Nimbus is a cloud dev environment that saves devs time and effort. Devs never set up or maintain their environment and get more performance via powerful cloud machines accessed through their local VS code app (with other IDEs to come!).',
+  'Amazing product and a 1click setup. No hastle. I have hd some quite advanced use cases like running a whole k8s cluster on my ninbus and developing inside pods - and it just worked, without any special considerations. My Mac laptop now has 17 hours of battery life, and I have never heard fans, all while literally running my whole stack.',
+  '/img/avatar/marko.jpeg',
+  '/img/product-logo/nimbus_logo.jpeg'
+  ),
+  (
+  'Appliku',
+  8,
+  10,
+  'Appliku automagically sets up your server and runs your web apps from GitHub or GitLab. Set up in under 5 minutes, click to deploy. Think of Appliku as your personal DevOps.',
+  null,
+  null,
+  '/img/product-logo/appliku_logo.jpeg'
+  ),
+  (
+  'Codiva',
+  1,
+  8,
+  'Codiva is an online code compiler and IDE for C, C++ and Java targeted for students learning to code or practice interview problems. You can get started instantly. No installation or setup needed. Codiva saves you a lot of time because it compiles as you type, with autocomplete. It also makes it easy to share your code online and on your blogs.',
+  null,
+  null,
+  '/img/product-logo/codiva_logo.jpeg'
+  ),
+  (
+  'WebDen',
+  5,
+  10,
+  'WebDen is an open source mobile and tablet friendly code playground. It is a fully client-side app so you can install it as a progressive web app (PWA) on your device to use offline.',
+  null,
+  null,
+  '/img/product-logo/webden_logo.jpeg'
+  ),
+  (
+  'Codeshow',
+  6,
+  8,
+  'A Badass Platform For Sharing RealTime Code With Your Team. A CodeShow Has An Collaborative Editor Which Helps You To Share Your Real Time Code.',
+  null,
+  null,
+  '/img/product-logo/codeshow_logo.jpeg'
+  ),
+  (
+  'ReposTimeline',
+  null,
+  9,
+  'ReposTimeline is an open-sourced web app that lets you generate a stunning timeline of your GitHub public projects. Enter your username, and just like that, you get a beautiful visualization you can easily share.',
+  null,
+  null,
+  '/img/product-logo/repostimeline_logo.jpeg'
+  ),
+  (
+  'Dockside (Open-Source)',
+  1,
+  9,
+  'Dockside is an open-source tool for provisioning lightweight access-controlled IDEs, staging environments and sandboxes - aka devtainers - on local machines, on-premises (raw metal or VM) or in the cloud.',
+  null,
+  null,
+  '/img/product-logo/dockside_open_source_logo.jpeg'
+  ),
+  (
+  'Track Awesome List',
+  4,
+  4,
+  'Track your favorite github awesome repo updates, not just star it. We track 500+ most popular awesome repos, so you can get the awesome updates daily or weekly. To provide a nice reading experience, we show the star count of each github repo next to it.',
+  null,
+  null,
+  '/img/product-logo/track_awesome_list_logo.jpeg'
+  ),
+  (
+  'RunPython.Live',
+  1,
+  9,
+  'RunPython.Live is an online Python IDE which can be run in browser. You do not need to download and install Python on your computer, just run code in https:RunPython.Live simply. For now it can run standard Python modules include the Turtle module.',
+  'This is a great project for learning python programming. I am using it in my classroom. I would love to know the tech stack behind and the names of the creators for giving the credits they deserve!',
+  '/img/avatar/giannis.jpeg',
+  '/img/product-logo/runpython_live_logo.jpeg'
+  ),
+  (
+  'Optic',
+  null,
+  6,
+  'Optic is an AI that automates the routine parts of programming so developers can focus on their most important work. Optic saves 15 hours per developer per week. Teams can reclaim time by automating the parts of development they enjoy least.',
+  null,
+  null,
+  '/img/product-logo/optic_logo.jpeg'
+  ),
+  (
+  'GitHub Open With',
+  1,
+  9,
+  'Open your Github repository with 3rd party web IDEs When navigating to any github repository, you can click on the green code button and see new options: - Open in Github1s - Open in StackBlitz - Open in CodeSandbox',
+  null,
+  null,
+  '/img/product-logo/github_open_with_logo.jpeg'
+  ),
+  (
+  'Jott',
+  null,
+  785,
+  'The fastest way to collaborate on designs and images',
+  null,
+  null,
+  '/img/product-logo/jott_logo.jpeg'
+  ),
+  (
+  'repostatus',
+  null,
+  12,
+  'Repostatus runs a sentiment analysis engine on various aspects of your repo in order to calculate the happiness of your repo. We support both private and public repos',
+  null,
+  null,
+  '/img/product-logo/repostatus_logo.jpeg'
+  ),
+  (
+  'CodeSync',
+  3,
+  7,
+  'CodeSync Provide Collebrative Code-Editor, Real-Time Audio Communication, Collaborative Drawing board for explaining an idea, Collaborative text-editor for taking notes, Real-Time chat box for chatting with other room members and Many more things with smooth UI.',
+  null,
+  null,
+  '/img/product-logo/codesync_logo.jpeg'
+  ),
+  (
+  'Locomogo',
+  null,
+  9,
+  'LoCoMoGo is the toy train that teaches children coding through play. Now available on Indiegogo – limited seats left!',
+  null,
+  null,
+  '/img/product-logo/locomogo_logo.jpeg'
+  ),
+  (
+  'devenv',
+  null,
+  2,
+  'Fast, Declarative, Reproducible, and Composable Developer Environments',
+  null,
+  null,
+  '/img/product-logo/devenv_logo.jpeg'
   );
